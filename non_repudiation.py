b8560e151d6a8d20e378909be9090e94dbca04f0
Roberto Polli <robipolli@gmail.com>
2018-06-07 14:36:04 +0200

    wip

--- non_repudiation.py
+++ non_repudiation.py
@@ -3,6 +3,8 @@ from jose import jws
 from Crypto.PublicKey import RSA
 from os.path import isfile
 import time
+from cryptography import x509
+from cryptography.hazmat.backends import default_backend
 
 from hashlib import sha256
 
@@ -17,12 +19,38 @@ header = {
 }
 
 
-def create_key(fpath):
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.asymmetric import rsa, ec
+from cryptography.hazmat.primitives.serialization import Encoding, PrivateFormat, NoEncryption
+
+import OpenSSL
+
+def generate_ecdsa_key(key_curve='secp256r1'):
+    key_curve = key_curve.lower()
+    if ('secp256r1' == key_curve):
+        key = ec.generate_private_key(ec.SECP256R1(), default_backend())
+    elif ('secp384r1' == key_curve):
+        key = ec.generate_private_key(ec.SECP384R1(), default_backend())
+    elif ('secp521r1' == key_curve):
+        key = ec.generate_private_key(ec.SECP521R1(), default_backend())
+    else:
+        raise NotImplementedError('Unsupported key curve: ' + key_curve)
+    key_pem = key.private_bytes(encoding=Encoding.PEM, format=PrivateFormat.TraditionalOpenSSL, encryption_algorithm=NoEncryption())
+    return OpenSSL.crypto.load_privatekey(OpenSSL.crypto.FILETYPE_PEM, key_pem)
+
+
+def create_key(fpath, algorithm='ECDSA'):
     if isfile(fpath + ".key"):
         return
-    key = RSA.generate(2048)
+    if algorithm == 'ECDSA':
+        f = generate_ecdsa_key
+    elif algorithm == 'RSA':
+        f = lambda : RSA.generate(2048).exportKey("PEM")
+    else:
+        raise NotImplementedError("Allowed algorithms: RSA, ECDSA")
+    
     with open(fpath + ".key", 'wb') as fh:
-        fh.write(key.exportKey("PEM"))
+        fh.write(f())
 
     with open(fpath + ".pub", 'wb') as fh:
         fh.write(key.publickey().exportKey('PEM'))
@@ -46,6 +74,13 @@ def sign_header(fpath, hdr, x5c=False):
 def hash_body(body):
     return sha256(body).hexdigest()
 
+
+def get_certificate_from_pem(pem_file):
+    backend = default_backend()
+    with open(pem_file, 'rb') as f:
+        crt_data = f.read()
+        return x509.load_pem_x509_certificate(crt_data, backend)
+    
 
 def cli(url="http://localhost:8080/ping", data=None):
     """A simple client validating Non-Repudiation header."""
